afae5e9050213182d0777263efa6ab338f8ce8ba
Iommi <daniel.markos666@gmail.com>
2025-07-12 00:55:11 +0000

    Carta 1 completa e corrigida

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,29 +2,39 @@
 
 
 int main() {
-    //variáveis carta 1 e 2
+    //variáveis carta 1
         int populacao, pontosturisticos;
             char cidade [70], code [3];
-            char estado [1];
+            char estado [10];
                 float pib, area;
 
                     //cadastramento da carta 1
 
                         printf("Digite uma letra entre A e H para um estado: \n");
-                            scanf("%c", estado);
+                            scanf("%s", estado);
 
                                 printf("Código da carta: \n");
                                     scanf("%s", code);
 
                                         printf("Nome da cidade: \n");
-                                            scanf("%50s", cidade);
+                                            scanf("%s", cidade);
 
                                                 printf("População: \n");
                                                     scanf("%d", &populacao);
 
                                                         printf("Área em KM²: \n");
                                                             scanf("%f", &area);
-    
+
+                                                            printf("Carta1: \n");
+                                                            printf("Estado: %s\n", estado);
+                                                            printf("Código da carta: %s\n", code);
+                                                            printf("Nome da cidade: %s\n", cidade);
+                                                            printf("População: %d\n", populacao);
+                                                            printf("Área em KM²: %f\n", area);
+
+                                                        
+
+                                        
 
 
     return 0;
